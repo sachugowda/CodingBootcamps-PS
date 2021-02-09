82d3645276be88d8e24161f013e422f9aeaddb3e
Sachin DN <sachugowda@hotmail.com>
2021-02-09 11:22:13 +0530

    First Program

--- Program_1.c
+++ Program_1.c
@@ -6,5 +6,10 @@ int main()
 {
 TYPE num = 100;
 TYPE letter = A;
+/* Printing num and charecter value */
+printf("%d\n",num);
+printf("%c\n",letter);
+/* if you done creerct you will get bellow message */
+printf("Good Job...");
 return 0;
 }
