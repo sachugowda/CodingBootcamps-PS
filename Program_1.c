198498d3fba2dcb04c15bb838fa568e41fb79ecf
Sachin DN <sachugowda@hotmail.com>
2021-02-09 11:12:02 +0530

    Create Program_1.c
    Updated Program

---
+++ Program_1.c
@@ -0,0 +1,10 @@
+/* Find the suitable data type for the below values and write them by replacing
+the word &quot;TYPE&quot;. */
+
+#include<stdio.h>
+int main()
+{
+TYPE num = 100;
+TYPE letter = A;
+return 0;
+}
