90c4b92b96b180a81fee26d8904e0dc92edd8289
Sachin DN <sachugowda@hotmail.com>
2021-02-09 11:52:27 +0530

    Update Program_2.c

--- Program_2.c
+++ Program_2.c
@@ -1,6 +1,6 @@
 //Declare two variables to store age and weight. and also assgin your age and weight values to those variables (Note: The variable name for age should be age and weight should be weight)
 
-#include&lt;stdio.h&gt;
+#include<stdio.h>
 int main()
 {
 //Replace Your Code and values in bellow two lines
