371493a6b7dddb834e8511eb2f8aefdcf1f64b29
Sachin DN <sachugowda@hotmail.com>
2021-02-09 11:50:50 +0530

    Variables Program

---
+++ Program_2.c
@@ -0,0 +1,14 @@
+//Declare two variables to store age and weight. and also assgin your age and weight values to those variables (Note: The variable name for age should be age and weight should be weight)
+
+#include&lt;stdio.h&gt;
+int main()
+{
+//Edit below this line
+int "Replace Variable Name" = "Your age";
+float "Replace Varibale name" = "Your Weight";
+
+//If you done correct you will get this meessage 
+printf("Good Job..");
+
+return 0;
+}
